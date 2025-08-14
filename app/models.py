20a17f2060249ada84cbef7cc039bc6199441aab
moortje <kbc617@hotmail.com>
2025-08-14 20:18:41 +0900

    Set up a temp db for user

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(32), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, flash, redirect, render_template, request, session
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.models import User
 from app import db
@@ -32,22 +32,22 @@ def login():
             return "Please enter password", 400
 
         # Query database for username
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = ?", username
-        )
+        # User is sqlalchemy model representing users table
+        user = User.query.filter_by(username=username).first()
 
-        # Ensure username exists and password is correct
-        # A hash is the result of a hash function that outputs a seemingly random string for an input
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], password
-        ):
-            return apology("invalid username and/or password", 400)
+        # Ensure username was submitted
+        if not user:
+            return "Invalid id", 400
+        
+        # Ensure correct password was submitted
+        if not check_password_hash(user.password_hash, password):
+            return "Invalid password", 400
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        # Set user_id session
+        session["user_id"] = user.id
 
-        # Remember user name
-        session["user_name"] = rows[0]["username"]
+        # Set user_name session
+        session["user_name"] = user.username
 
         # Redirect user to home page
         return redirect("/")
@@ -66,8 +66,20 @@ def register():
         
         # Ensure username was submitted
         if not username:
-            return apology("must provide username", 400)
-                
+            return "Please enter username", 400
+   
+        # Ensure password was submitted
+        if not password:
+            return "Please enter password", 400
+        
+        # Ensure confirmation was submitted
+        if not confirmation:
+            return "Please enter confirmation", 400
+        
+        # Ensure password and comfirmation match
+        if password != confirmation:
+            return "Please match password and password confirmation", 400
+
         
         # Check if user exists
         existing_user = User.query.filter_by(username=username).first()
@@ -76,17 +88,7 @@ def register():
             flash('Username already taken')
             return redirect('/register')
 
-        if not password:
-            flash("Must provide password")
-            return redirect("/register")
 
-        if not confirmation:
-            flash("Must provide password confirmation")
-            return redirect("/register")
-
-        if password != confirmation:
-            flash("Password and confirmation must match")
-            return redirect("/register")
 
         # Create new user
 
@@ -96,12 +98,12 @@ def register():
             password_hash=generate_password_hash(password)
         )
 
-        
+
         db.session.add(new_user)
         db.session.commit()
 
-        # Redirect user to home page
-        return redirect("/")
+        flash('Registered successfully!')
+        return redirect('/login')
 
     # User reached route via GET (as by clicking a link or via redirect)
     return render_template("register.html")
